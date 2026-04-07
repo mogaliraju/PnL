852bd51a3bcf4723d5a76d93a418dd6438c05724
Mogaliraju Srikar <srikar.mogaliraju@ctrls.in>
2026-04-07 19:03:25 +0530

    Update PythonAnywhere WSGI path

--- wsgi.py
+++ wsgi.py
@@ -2,10 +2,10 @@ import sys
 import os
 
 # Add your app directory to path
-path = '/home/automatonsx/PnL'
+path = '/home/SrikarMogaliraju/PnL'
 if path not in sys.path:
     sys.path.insert(0, path)
 
-os.environ['DATA_DIR'] = '/home/automatonsx/PnL/userdata'
+os.environ['DATA_DIR'] = '/home/SrikarMogaliraju/PnL/userdata'
 
 from app import app as application
